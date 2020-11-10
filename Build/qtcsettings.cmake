6d9a83c297124907433181d7005e7b18d90356a5
Poscowekh <56505043+Poscowekh@users.noreply.github.com>
2020-11-10 12:33:10 +0300

    cleaned up

--- Build/qtcsettings.cmake
+++
@@ -1,7 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_CXX_COMPILER" "c:/users/1tira/gcc/bin/g++.exe" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "c:/users/1tira/gcc/bin/gcc.exe" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "" CACHE "STRING" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "" CACHE "STRING" "" FORCE)
